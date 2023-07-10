5363ecc8a041d6d400e9678b903c2a8b27c39113
teddy <teddy.gauthier@outlook.com>
2023-07-10 17:08:39 +0200

    [fix] try to fix controller

--- user-api/src/app.controller.ts
+++ user-api/src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { GrpcMethod } from '@nestjs/microservices';
+import { Metadata } from '@grpc/grpc-js';
+import { HeroCRUDServiceControllerMethods } from './stubs/user/v1/user';
 
 @Controller()
+@HeroCRUDServiceControllerMethods()
 export class AppController {
     constructor(private readonly appService: AppService) {}
-
-    @Get()
-    getHello(): string {
-        return this.appService.getHello();
-    }
 }
