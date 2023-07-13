319b013bcd22fa96d32c32acac39c89d8bd64cf0
teddy <teddy.gauthier@outlook.com>
2023-07-13 16:16:13 +0200

    [feat] finish user-api

--- user-api/src/stubs/user/v1/user.ts
+++ user-api/src/stubs/user/v1/user.ts
@@ -1,139 +1,102 @@
 /* eslint-disable */
-import { Metadata } from '@grpc/grpc-js';
-import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices';
-import { Observable } from 'rxjs';
+import { Metadata } from "@grpc/grpc-js";
+import { GrpcMethod, GrpcStreamMethod } from "@nestjs/microservices";
+import { Observable } from "rxjs";
 
-export const protobufPackage = 'hero.v1alpha';
+export const protobufPackage = "user.v1";
 
 export interface User {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
+  id: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
 export interface UserServiceGetRequest {
-    id: string;
+  id: string;
+  name: string;
 }
 
 export interface UserServiceGetResponse {
-    user: User[];
+  users: User[];
 }
 
 export interface UserServiceAddRequest {
-    name: string;
-    email: string;
-    password: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
 export interface UserServiceAddResponse {
-    user: User | undefined;
+  user: User | undefined;
 }
 
 export interface UserServiceUpdateRequest {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
+  id: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
 export interface UserServiceUpdateResponse {
-    user: User | undefined;
+  user: User | undefined;
 }
 
 export interface UserServiceDeleteRequest {
-    id: string;
+  id: string;
 }
 
 export interface UserServiceDeleteResponse {
-    user: User | undefined;
+  user: User | undefined;
 }
 
-export const HERO_V1ALPHA_PACKAGE_NAME = 'hero.v1alpha';
+export const USER_V1_PACKAGE_NAME = "user.v1";
 
 export interface UserServiceClient {
-    get(
-        request: UserServiceGetRequest,
-        metadata?: Metadata,
-    ): Observable<UserServiceGetResponse>;
+  get(request: UserServiceGetRequest, metadata?: Metadata): Observable<UserServiceGetResponse>;
 
-    add(
-        request: UserServiceAddRequest,
-        metadata?: Metadata,
-    ): Observable<UserServiceAddResponse>;
+  add(request: UserServiceAddRequest, metadata?: Metadata): Observable<UserServiceAddResponse>;
 
-    update(
-        request: UserServiceUpdateRequest,
-        metadata?: Metadata,
-    ): Observable<UserServiceUpdateResponse>;
+  update(request: UserServiceUpdateRequest, metadata?: Metadata): Observable<UserServiceUpdateResponse>;
 
-    delete(
-        request: UserServiceDeleteRequest,
-        metadata?: Metadata,
-    ): Observable<UserServiceDeleteResponse>;
+  delete(request: UserServiceDeleteRequest, metadata?: Metadata): Observable<UserServiceDeleteResponse>;
 }
 
 export interface UserServiceController {
-    get(
-        request: UserServiceGetRequest,
-        metadata?: Metadata,
-    ):
-        | Promise<UserServiceGetResponse>
-        | Observable<UserServiceGetResponse>
-        | UserServiceGetResponse;
+  get(
+    request: UserServiceGetRequest,
+    metadata?: Metadata,
+  ): Promise<UserServiceGetResponse> | Observable<UserServiceGetResponse> | UserServiceGetResponse;
 
-    add(
-        request: UserServiceAddRequest,
-        metadata?: Metadata,
-    ):
-        | Promise<UserServiceAddResponse>
-        | Observable<UserServiceAddResponse>
-        | UserServiceAddResponse;
+  add(
+    request: UserServiceAddRequest,
+    metadata?: Metadata,
+  ): Promise<UserServiceAddResponse> | Observable<UserServiceAddResponse> | UserServiceAddResponse;
 
-    update(
-        request: UserServiceUpdateRequest,
-        metadata?: Metadata,
-    ):
-        | Promise<UserServiceUpdateResponse>
-        | Observable<UserServiceUpdateResponse>
-        | UserServiceUpdateResponse;
+  update(
+    request: UserServiceUpdateRequest,
+    metadata?: Metadata,
+  ): Promise<UserServiceUpdateResponse> | Observable<UserServiceUpdateResponse> | UserServiceUpdateResponse;
 
-    delete(
-        request: UserServiceDeleteRequest,
-        metadata?: Metadata,
-    ):
-        | Promise<UserServiceDeleteResponse>
-        | Observable<UserServiceDeleteResponse>
-        | UserServiceDeleteResponse;
+  delete(
+    request: UserServiceDeleteRequest,
+    metadata?: Metadata,
+  ): Promise<UserServiceDeleteResponse> | Observable<UserServiceDeleteResponse> | UserServiceDeleteResponse;
 }
 
 export function UserServiceControllerMethods() {
-    return function (constructor: Function) {
-        const grpcMethods: string[] = ['get', 'add', 'update', 'delete'];
-        for (const method of grpcMethods) {
-            const descriptor: any = Reflect.getOwnPropertyDescriptor(
-                constructor.prototype,
-                method,
-            );
-            GrpcMethod('UserService', method)(
-                constructor.prototype[method],
-                method,
-                descriptor,
-            );
-        }
-        const grpcStreamMethods: string[] = [];
-        for (const method of grpcStreamMethods) {
-            const descriptor: any = Reflect.getOwnPropertyDescriptor(
-                constructor.prototype,
-                method,
-            );
-            GrpcStreamMethod('UserService', method)(
-                constructor.prototype[method],
-                method,
-                descriptor,
-            );
-        }
-    };
+  return function (constructor: Function) {
+    const grpcMethods: string[] = ["get", "add", "update", "delete"];
+    for (const method of grpcMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcMethod("UserService", method)(constructor.prototype[method], method, descriptor);
+    }
+    const grpcStreamMethods: string[] = [];
+    for (const method of grpcStreamMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcStreamMethod("UserService", method)(constructor.prototype[method], method, descriptor);
+    }
+  };
 }
 
-export const USER_SERVICE_NAME = 'UserService';
+export const USER_SERVICE_NAME = "UserService";
