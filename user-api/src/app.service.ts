9b40faadf21f3c0f6e8216d6e44ffdcde6878253
teddy <teddy.gauthier@outlook.com>
2023-07-15 01:35:21 +0200

    [feat] add grpc on auth-api

--- user-api/src/app.service.ts
+++ user-api/src/app.service.ts
@@ -6,27 +6,33 @@ import { Prisma } from '@prisma/client';
 @Injectable()
 export class AppService {
     constructor(private prisma: PrismaService) {}
+
     create(data: Prisma.UserCreateInput): Promise<User> {
         return this.prisma.user.create({ data });
     }
+
     findAll(): Promise<User[]> {
         return this.prisma.user.findMany();
     }
+
     delete(id: string): Promise<User> {
         return this.prisma.user.delete({
             where: { id },
         });
     }
+
     findById(id: string): Promise<User> {
         return this.prisma.user.findUnique({
             where: { id },
         });
     }
+
     findByName(name: string): Promise<User> {
         return this.prisma.user.findUnique({
             where: { name },
         });
     }
+
     update(id: string, data: Prisma.UserUpdateInput): Promise<User> {
         return this.prisma.user.update({
             where: { id },
